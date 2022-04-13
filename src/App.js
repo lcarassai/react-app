3269d763f3774e1051738b45222b650cfc4b2a98
lcarassai <laracarassai@gmail.com>
2022-04-12 23:29:08 -0300

    cambie parrafo

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hola React</h1>
-      {/*       <header className="App-header">
+
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>Hola React</h1>
         </p>
         <a
           className="App-link"
@@ -18,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-      </header> */}
+      </header>
     </div>
   );
 }
